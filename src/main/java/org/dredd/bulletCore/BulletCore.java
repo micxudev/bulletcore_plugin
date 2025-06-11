6eb0937a86634353303f333b698b2d5be5dd65d5
dredd <1misha.baltovsky@gmail.com>
2025-06-11 11:24:15 +0200

    initial plugin setup

---
+++ src/main/java/org/dredd/bulletCore/BulletCore.java
@@ -0,0 +1,16 @@
+package org.dredd.bulletCore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BulletCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
