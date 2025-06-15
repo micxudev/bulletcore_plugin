a108d93fa40a862db2d049442ea213a4d3a88f95
dredd <1misha.baltovsky@gmail.com>
2025-06-15 15:29:59 +0200

    enhance exceptions with serialVersionUID and additional ctors

--- src/main/java/org/dredd/bulletcore/custom_item_manager/exceptions/ItemLoadException.java
+++ src/main/java/org/dredd/bulletcore/custom_item_manager/exceptions/ItemLoadException.java
@@ -1,5 +1,7 @@
 package org.dredd.bulletcore.custom_item_manager.exceptions;
 
+import java.io.Serial;
+
 /**
  * Exception thrown when a custom item fails to load properly from configuration or data source.
  *
@@ -11,11 +13,36 @@ package org.dredd.bulletcore.custom_item_manager.exceptions;
 public class ItemLoadException extends Exception {
 
     /**
+     * Used during deserialization to verify that the sender and receiver of a serialized object have loaded classes for that object that are compatible with respect to serialization.
+     */
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    /**
      * Constructs a new {@code ItemLoadException} with the specified detail message.
      *
      * @param message the detail message describing the reason for the failure
      */
     public ItemLoadException(String message) {
         super(message);
+    }
+
+    /**
+     * Constructs a new {@code ItemLoadException} with the specified detail message and cause.
+     *
+     * @param message the detail message
+     * @param cause   the cause of this exception
+     */
+    public ItemLoadException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     * Constructs a new {@code ItemLoadException} with the specified cause.
+     *
+     * @param cause the cause of this exception
+     */
+    public ItemLoadException(Throwable cause) {
+        super(cause);
     }
 }
