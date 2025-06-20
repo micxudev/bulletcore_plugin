c7a4b5bbe8890d4f2dfc1ddd596c830d807dd60b
dredd <1misha.baltovsky@gmail.com>
2025-06-20 14:57:34 +0200

    add method for actions onDrop triggered using key (Q)

--- src/main/java/org/dredd/bulletcore/models/weapons/Weapon.java
+++ src/main/java/org/dredd/bulletcore/models/weapons/Weapon.java
@@ -17,6 +17,17 @@ public class Weapon extends CustomBase {
         super(attrs);
     }
 
+    /**
+     * Triggered when a player attempts to drop a weapon.
+     * This method is invoked specifically when the drop action is initiated manually with the drop key (Q).
+     *
+     * @param player   the player who attempts to drop the weapon
+     * @param usedItem the item being dropped, which is this weapon object
+     */
+    public void onDrop(@NotNull Player player, @NotNull ItemStack usedItem) {
+        //System.out.println("Weapon drop attempt on key (Q)");
+    }
+
     @Override
     public @NotNull ItemStack createItemStack() {
         // Add only weapon-specific attributes
