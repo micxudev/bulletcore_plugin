5f21e17eac0a1bd9d9705d7bd62bf32fff8b8d2f
dredd <1misha.baltovsky@gmail.com>
2026-01-27 20:12:47 +0100

    comment `CurrentHit` record with Javadoc

--- src/main/java/org/dredd/bulletcore/models/weapons/shooting/CurrentHit.java
+++ src/main/java/org/dredd/bulletcore/models/weapons/shooting/CurrentHit.java
@@ -4,6 +4,15 @@ import java.util.UUID;
 
 import org.dredd.bulletcore.models.weapons.Weapon;
 
+/**
+ * Data container representing a hit that is currently being processed.
+ *
+ * @param damager the UUID of the player damaging the victim
+ * @param victim  the UUID of the entity being damaged
+ * @param weapon  the weapon used to damage the victim
+ * @author dredd
+ * @since 1.0.0
+ */
 public record CurrentHit(
     UUID damager,
     UUID victim,
